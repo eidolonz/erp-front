5e01db32df07177a90248f6db343968075e7ef41
Peerapon Jumruswittayawong <j.peerapon@gmail.com>
2015-10-25 13:53:24 +0700

    test

--- js/app.js
+++ js/app.js
@@ -1,6 +1,46 @@
+
 function mainController($scope) {
   $scope.message = "Hello AngularJS";
   $scope.name = '';
+  $scope.purchaseOrder = [
+    {
+      "number": "PD00001",
+      "supplierName": "Supplier 1",
+      "orderDate": "September 16, 2015",
+      "deliverDate": "September 25, 2015",
+      "status": "Open",
+      "action": "[View]"
+
+            
+    },
+    {
+      "number": "PD00002",
+      "supplierName": "Supplier 4",
+      "orderDate": "September 16, 2015",
+      "deliverDate": "September 25, 2015",
+      "status": "Open",
+      "action": "[View]"
+            
+    },
+    {
+      "number": "PD00003",
+      "supplierName": "Supplier 2",
+      "orderDate": "September 16, 2015",
+      "deliverDate": "September 25, 2015",
+      "status": "Open",
+      "action": "[View]"
+            
+    },
+    {
+      "number": "PD00004",
+      "supplierName": "Supplier 2",
+      "orderDate": "September 19, 2015",
+      "deliverDate": "September 25, 2015",
+      "status": "Close",
+      "action": "[View]"
+            
+    }
+  ];
   $scope.pirates = [
     {
       "id": 1,
@@ -40,8 +80,52 @@ function custCtrl($scope, $http) {
   .success(function (response) {$scope.names = response.records;});
 };
 
+function getPO(){
+	this.purchaseOrder = po;
+};
+
 var app = angular.module('myApp', []);
 app.controller('MainController', mainController);
 app.controller('CustomersCtrl', custCtrl);
+app.controller('PoController', getPO);
 
+ 
+var po = [
+    {
+      "number": "PD00001",
+      "supplierName": "Supplier 1",
+      "orderDate": "September 16, 2015",
+      "deliverDate": "September 25, 2015",
+      "status": "Open",
+      "action": "[View]"
 
+            
+    },
+    {
+      "number": "PD00002",
+      "supplierName": "Supplier 4",
+      "orderDate": "September 16, 2015",
+      "deliverDate": "September 25, 2015",
+      "status": "Open",
+      "action": "[View]"
+            
+    },
+    {
+      "number": "PD00003",
+      "supplierName": "Supplier 2",
+      "orderDate": "September 16, 2015",
+      "deliverDate": "September 25, 2015",
+      "status": "Open",
+      "action": "[View]"
+            
+    },
+    {
+      "number": "PD00004",
+      "supplierName": "Supplier 2",
+      "orderDate": "September 19, 2015",
+      "deliverDate": "September 25, 2015",
+      "status": "Close",
+      "action": "[View]"
+            
+    }
+  ];
