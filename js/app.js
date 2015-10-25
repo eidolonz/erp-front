8ca157f8ecab810c305d0144cf4be3c91bd30a26
Peerapon Jumruswittayawong <j.peerapon@gmail.com>
2015-10-25 14:11:12 +0700

    dummy PO

--- js/app.js
+++ js/app.js
@@ -2,35 +2,8 @@
 function mainController($scope) {
   $scope.message = "Hello AngularJS";
   $scope.name = '';
-  $scope.purchaseOrder = [
-    {
-      "number": "PD00001",
-      "supplierName": "Supplier 1",
-      "orderDate": "September 16, 2015",
-      "deliverDate": "September 25, 2015",
-      "status": "Open",
-      "action": "[View]"
-
-            
-    },
-    {
-      "number": "PD00002",
-      "supplierName": "Supplier 4",
-      "orderDate": "September 16, 2015",
-      "deliverDate": "September 25, 2015",
-      "status": "Open",
-      "action": "[View]"
-            
-    },
-    {
-      "number": "PD00003",
-      "supplierName": "Supplier 2",
-      "orderDate": "September 16, 2015",
-      "deliverDate": "September 25, 2015",
-      "status": "Open",
-      "action": "[View]"
-            
-    },
+  $scope.purchaseOrder = po;
+  $scope.pirates = [
     {
       "number": "PD00004",
       "supplierName": "Supplier 2",
@@ -38,15 +11,6 @@ function mainController($scope) {
       "deliverDate": "September 25, 2015",
       "status": "Close",
       "action": "[View]"
-            
-    }
-  ];
-  $scope.pirates = [
-    {
-      "id": 1,
-      "username": "goldroger",
-      "name": "Gol D. Roger",
-      "position": "Pirate King"
     },
     {
       "id": 2,
@@ -81,7 +45,8 @@ function custCtrl($scope, $http) {
 };
 
 function getPO(){
-	this.purchaseOrder = po;
+	$http.get("http://www.w3schools.com/angular/customers.php")
+  .success(function (response) {$scope.names = response.records;});
 };
 
 var app = angular.module('myApp', []);
