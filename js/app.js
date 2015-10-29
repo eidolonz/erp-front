17544ece57db805d648c75e8d7a131c6b0b0382d
piyawatton <ton.chiniji@hotmail.com>
2015-10-30 00:42:05 +0700

    PD_UI
    done UI PD

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 var app = angular.module('myApp', []);
 app.controller('PoController', controllerGetPO);
 app.controller('SupplierController', supplierController);
-
+app.controller('InventoryController', controllerGetAI);
 
 function getPO($scope, $http){
 	alert("hey");
@@ -60,3 +60,25 @@ function supplierController($scope, $http){
 
   $scope.getSuppliers();
  }
+
+
+ // _________________AI______________________
+ function controllerGetAI($scope, $http){
+  
+  $scope.getpo = function(){
+    url = "http://54.179.174.140/api/inventory/search";
+    zone_id = $('input[name="zone"]').val();
+    product_type = $('input[name="product_type"]').val();
+    product_name = $('input[name="product_name"]').val();
+    product_status = $('select[name="product_status"]').val();
+    url = url + "?zone_id=" + po_id + "&order_date=" + product_type + "&sp_name=" + sp_name + "&po_status=" + po_status;
+    $http.get(url)
+      .success(function (response) {
+        $scope.inventory = response;
+        console.log(response);
+      });
+  };
+
+  $scope.getpo();
+
+}
